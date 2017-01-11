380c5ba24534fec7c47794360f10da9feb7e44eb
John Manero <jmanero@rapid7.com>
2017-01-11 15:25:49 -0500

    Add tests for helper rouines in main
    * Change log.Fatal to log.Panic in main.fatal to allow for recovery if one _really_ wants to
    * Fix formatting

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import "flag"
 import "log"
+import "os"
 import "strings"
 
 /**
@@ -9,33 +10,52 @@ import "strings"
  */
 func fatal(err error) {
 	if err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
 }
 
+/**
+ * Parse the certificates command flag and add to the handler
+ */
+func loadCertificates(flag string, handler *Verifier) (err error) {
+	// Use the default Amazon certificate if none are specified in command arguments
+	if len(flag) == 0 {
+		log.Printf("Using default Amazon AWS signing certificate")
+
+		_, err = handler.AddPEMCertificate(AmazonAWSCloudSigner)
+		return err
+	}
+
+	// Load certificates from specified files
+	for _, path := range strings.Split(flag, ",") {
+		if len(path) == 0 {
+			continue
+		}
+
+		_, err = handler.ReadPEMCertificate(path)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
 func main() {
+	var certs = flag.String("certificates", "", "Comma-separated list of paths to certificates used to verify signatures")
 	var socket = flag.String("socket", "./aws-verify.sock", "UNIX socket that verifier will listen on")
-	var certificates = flag.String("certificates", "", "Comma-separated list of paths to certificates used to verify signatures")
+	var mode = flag.Uint("mode", 0700, "File mode for listener socket")
+
 	flag.Parse()
 
 	handler := CreateVerifier()
 
-	// Use the default Amazon certificate if none are specified in command arguments
-	if len(*certificates) == 0 {
-		log.Printf("Using default Amazon AWS signing certificate")
-		_, err := handler.AddPEMCertificate(AMAZON_PUBLIC_CLOUD)
+	fatal(
+		loadCertificates(*certs, handler),
+	)
 
-		fatal(err)
-	}
-
-	// Load certificates from specified file
-	for _, path := range strings.Split(*certificates, ",") {
-		if len(path) > 0 {
-			_, err := handler.ReadPEMCertificate(path)
-
-			fatal(err)
-		}
-	}
-
-	CreateServer(*socket, handler).Listen()
+	fatal(
+		CreateServer(*socket, os.FileMode(*mode), handler).Listen(),
+	)
 }
